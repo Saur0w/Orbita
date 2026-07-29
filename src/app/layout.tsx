449d1ff53ce625d2e3ec2820751c3c757b2f9831
saur0w <sthapliyal085@gmail.com>
2026-07-29 19:25:20 +0530

    feat: initialize root layout with Geist fonts and Lenis scroll provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import LenisProvider from "@/components/Lenis";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+      <LenisProvider>
+        {children}
+      </LenisProvider>
+      </body>
     </html>
   );
 }
