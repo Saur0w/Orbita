852688bbf8cb07b49a60ca378e0409586ac7012f
Saur0w <sthapliyal085@gmail.com>
2026-09-24 07:48:17 +0530

    feat: add Footer component with styles and integrate into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import styles from "./page.module.css";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styles from "./style.module.scss";
+import { Logo } from "../Icons/logo";
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            
+        </footer>
+    )
+}
